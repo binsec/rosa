c984d114b77783ca4c0c00ecf2411f1cc62a4e69
Dimitri Kokkonis <dimitri.kokkonis@cea.fr>
2024-03-03 18:27:22 +0100

    distance_metric.rs: add documentation

--- src/distance_metric.rs
+++ src/distance_metric.rs
@@ -1,15 +1,42 @@
+//! Distance metrics to be used when measuring similarity between traces.
+//!
+//! A distance metric is a fast algorithm that compares the same component of two traces to
+//! determine the "distance" (or similarity) between them. The available distance metrics are
+//! implemented here.
+
 use std::{fmt, str};
 
 use serde::{Deserialize, Serialize};
 
 use crate::error::RosaError;
 
+/// The available distance metrics.
 #[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 pub enum DistanceMetric {
+    /// The Hamming distance metric.
+    ///
+    /// This distance metric simply implements the [Hamming distance](
+    /// https://en.wikipedia.org/wiki/Hamming_distance).
     Hamming,
 }
 
 impl DistanceMetric {
+    /// Compute the distance between two vectors (e.g. components of runtime traces).
+    ///
+    /// # Arguments
+    /// * `v1` - The first vector.
+    /// * `v2` - The second vector.
+    ///
+    /// # Examples
+    /// ```
+    /// use rosa::distance_metric::DistanceMetric;
+    ///
+    /// let metric = DistanceMetric::Hamming;
+    /// assert_eq!(
+    ///     metric.dist(&[0, 0, 0, 1], &[1, 0, 0, 0]),
+    ///     2,
+    /// );
+    /// ```
     pub fn dist(&self, v1: &[u8], v2: &[u8]) -> u64 {
         match self {
             Self::Hamming => hamming(v1, v2),
@@ -40,6 +67,11 @@ impl str::FromStr for DistanceMetric {
     }
 }
 
+/// Compute the Hamming distance between two vectors.
+///
+/// # Arguments
+/// * `v1` - The first vector.
+/// * `v2` - The second vector.
 fn hamming(v1: &[u8], v2: &[u8]) -> u64 {
     assert_eq!(v1.len(), v2.len(), "vector length mismatch.");
 
