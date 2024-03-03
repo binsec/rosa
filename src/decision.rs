dc1b9f5642a5fd3bf3f2d3e822e49fcf0885c09f
Dimitri Kokkonis <dimitri.kokkonis@cea.fr>
2024-03-03 18:27:09 +0100

    decision.rs: add documentation

--- src/decision.rs
+++ src/decision.rs
@@ -1,32 +1,58 @@
+//! Oracle decision definition & utilities.
+//!
+//! In order to be able to analyze the decisions of oracle algorithms (see
+//! [rosa::oracle](crate::oracle)), a more complex decision structure is defined here, as well as
+//! some utility functions to handle it.
+
 use std::{fmt, fs, path::Path};
 
 use serde::{Deserialize, Serialize};
 
 use crate::error::RosaError;
 
+/// The reason for an oracle decision.
 #[derive(Serialize, Deserialize, Debug)]
 pub enum DecisionReason {
+    /// The decision was made because the trace was a seed trace (i.e. it originated from the seed
+    /// phase).
     Seed,
+    /// The decision was made because of the edges of the trace.
     Edges,
+    /// The decision was made because of the syscalls of the trace.
     Syscalls,
+    /// The decision was made because of both the edges and the syscalls of the trace.
     EdgesAndSyscalls,
 }
 
+/// The decision made by an oracle.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Decision {
+    /// The UID of the trace for which the decision was made.
     pub trace_uid: String,
+    /// The UID of the cluster the trace was compared with.
     pub cluster_uid: String,
+    /// If [true], the trace is determined to be associated with a backdoor; otherwise, it is
+    /// considered to be a normal trace.
     pub is_backdoor: bool,
+    /// The reason for the decision.
     pub reason: DecisionReason,
 }
 
+/// The timed decision made by an oracle.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TimedDecision {
+    /// The decision itself.
     pub decision: Decision,
+    /// The amount of seconds the decision was made in (counting from the very start of the
+    /// detection, i.e. the run phase).
     pub seconds: u64,
 }
 
 impl TimedDecision {
+    /// Load a decision from file.
+    ///
+    /// # Arguments
+    /// * `file` - The file to load the decision from.
     pub fn load(file: &Path) -> Result<Self, RosaError> {
         let decision_json = fs::read_to_string(file).map_err(|err| {
             error!(
@@ -40,6 +66,11 @@ impl TimedDecision {
             .map_err(|err| error!("could not deserialize decision JSON: {}.", err))
     }
 
+    /// Save the decision to a file.
+    ///
+    /// # Arguments
+    /// * `output_dir` - The output directory in which to save the decision file. The decision
+    ///   file's name will be the UID of the trace it's associated with (e.g. `id_000000.json`).
     pub fn save(&self, output_dir: &Path) -> Result<(), RosaError> {
         let decision_json =
             serde_json::to_string_pretty(&self).expect("failed to serialize decision JSON.");
