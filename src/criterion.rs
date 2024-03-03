94e636862eb3a13b19cd3e225c664e86a83969ab
Dimitri Kokkonis <dimitri.kokkonis@cea.fr>
2024-03-03 18:12:09 +0100

    criterion.rs: add documentation

--- src/criterion.rs
+++ src/criterion.rs
@@ -1,14 +1,29 @@
+//! Criteria to determine which component of a runtime trace should be taken into account
+//!
+//! Since runtime traces have two components (CFG edges and syscalls), there are multiple ways to
+//! take either (or both) into account, for example when computing the similarity between two
+//! traces. This is materialized through a **criterion**.
+
 use std::{fmt, str};
 
 use serde::{Deserialize, Serialize};
 
 use crate::error::RosaError;
 
+/// A criterion to describe which component(s) of the runtime trace are taken into account.
 #[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 pub enum Criterion {
+    /// Only the edge component of the runtime trace is taken into account.
     EdgesOnly,
+    /// Only the syscall component of the runtime trace is taken into account.
     SyscallsOnly,
+    /// Either the edge or the syscall component of the runtime trace is taken into account.
+    /// The meaning of this criterion depends on the application; in general, you should think of
+    /// it as a _logical OR_ between [Criterion::EdgesOnly] and [Criterion::SyscallsOnly].
     EdgesOrSyscalls,
+    /// Both the edge and the syscall components of the runtime trace are taken into account.
+    /// The meaning of this criterion depends on the application; in general, you should think of
+    /// it as a _logical AND_ between [Criterion::EdgesOnly] and [Criterion::SyscallsOnly].
     EdgesAndSyscalls,
 }
 
