f8dd35d57c143bb814ac6de96cbb8c7597f67897
Dimitri Kokkonis <dimitri.kokkonis@cea.fr>
2024-04-19 16:22:35 +0200

    rosa-showmap: also show syscalls

--- src/cli/rosa_showmap.rs
+++ src/cli/rosa_showmap.rs
@@ -5,7 +5,7 @@ use std::{
     process::ExitCode,
 };
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use colored::Colorize;
 
 use rosa::{error::RosaError, trace::Trace};
@@ -24,25 +24,77 @@ mod logging;
 struct Cli {
     /// The trace file to analyze.
     trace_file: PathBuf,
+
+    /// The component of the trace to show.
+    #[arg(
+        value_enum,
+        short = 'c',
+        long = "component",
+        default_value_t = Component::Edges,
+        value_name = "COMPONENT"
+    )]
+    component: Component,
 }
 
-fn run(file: &Path) -> Result<(), RosaError> {
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Component {
+    Edges,
+    Syscalls,
+    EdgesAndSyscalls,
+}
+
+fn run(file: &Path, component: Component) -> Result<(), RosaError> {
     Trace::load("_dummy", file, file).map(|trace| {
-        trace
+        let edges_output: Vec<String> = trace
             .edges
             .iter()
             .enumerate()
-            .for_each(|(index, edge)| match edge {
-                0 => {}
-                count => println!("{:06}:{}", index, count),
+            .filter_map(|(index, edge)| match edge {
+                0 => None,
+                count => Some(format!("{:06}:{}", index, count)),
             })
+            .collect();
+
+        let syscalls_output: Vec<String> = trace
+            .syscalls
+            .iter()
+            .enumerate()
+            .filter_map(|(index, syscall)| match syscall {
+                0 => None,
+                count => Some(format!("{:06}:{}", index, count)),
+            })
+            .collect();
+
+        match component {
+            Component::Edges => {
+                println!("{}", edges_output.join("\n"));
+            }
+            Component::Syscalls => {
+                println!("{}", syscalls_output.join("\n"));
+            }
+            Component::EdgesAndSyscalls => {
+                println!("EDGES");
+                println!("{}", edges_output.join("\n"));
+                println!("SYSCALLS");
+                println!("{}", syscalls_output.join("\n"));
+            }
+        }
     })
 }
 
+// Reset SIGPIPE, so that the output of rosa-showmap may be piped to other stuff.
+// See https://stackoverflow.com/q/65755853/.
+fn reset_sigpipe() {
+    unsafe {
+        libc::signal(libc::SIGPIPE, libc::SIG_DFL);
+    }
+}
+
 fn main() -> ExitCode {
+    reset_sigpipe();
     let cli = Cli::parse();
 
-    match run(&cli.trace_file) {
+    match run(&cli.trace_file, cli.component) {
         Ok(_) => ExitCode::SUCCESS,
         Err(err) => {
             println_error!(err);
